875b6e17813f6277984c904462b6444250863616
Tan0442 <ytsegai48@gmail.com>
2024-09-03 17:49:04 -0400

    Update PlayeController.cs

--- MarioCloneDemo/Assets/PlayeController.cs
+++ MarioCloneDemo/Assets/PlayeController.cs
@@ -9,7 +9,7 @@ public class PlayeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
