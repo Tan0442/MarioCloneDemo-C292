f23997afa087ed4837fbfcf46e794db08be5a635
Tan0442 <ytsegai48@gmail.com>
2024-09-03 17:46:57 -0400

    Lab2
    Lab02 work

---
+++ MarioCloneDemo/Assets/PlayeController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeController : MonoBehaviour
+{
+    private 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
